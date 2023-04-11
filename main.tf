129e7ee52d15c731ae1e53978104e156fc22903e
lalitha2407 <lalitha@bootlabstech.com>
2023-04-11 15:38:15 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "azurerm_mssql_server" "example" {
+  name                         = var.server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = var.server_version
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  connection_policy            = var.connection_policy
+}
+
+resource "azurerm_mssql_database" "test" {
+  name                           = var.name
+  server_id                      = azurerm_mssql_server.example.id
+  create_mode                    = var.create_mode
+  geo_backup_enabled             = var.geo_backup_enabled
+  maintenance_configuration_name = var.maintenance_configuration_name
+  max_size_gb                    = var.max_size_gb
+  sku_name                       = var.sku_name
+  storage_account_type           = var.storage_account_type
+  #   threat_detection_policy {
+  #     disabled_alerts = [ "value" ]
+  #     email_account_admins = 
+  #     email_addresses = 
+  #     retention_days = 
+  #     state = 
+  #     storage_account_access_key = 
+  #     storage_endpoint = 
+
+  #   }
+  #   collation      = "SQL_Latin1_General_CP1_CI_AS"
+  #   license_type   = "LicenseIncluded"
+  #   read_scale     = true
+  #   zone_redundant = true
+}
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "server_name" {
+  type        = string
+  description = "value"
+
+}
+variable "resource_group_name" {
+  type        = string
+  description = "value"
+
+}
+variable "location" {
+  type        = string
+  description = "value"
+
+}
+variable "server_version" {
+  type        = string
+  description = "value"
+  default     = "12.0"
+
+}
+variable "administrator_login" {
+  type        = string
+  description = "value"
+
+}
+variable "administrator_login_password" {
+  type        = string
+  description = "value"
+
+
+}
+variable "connection_policy" {
+  type        = string
+  description = "value"
+  default     = "Default"
+
+}
+variable "create_mode" {
+  type        = string
+  description = "value"
+  default     = "Default"
+
+
+}
+variable "geo_backup_enabled" {
+  type        = bool
+  description = "value"
+  default     = true
+
+}
+variable "maintenance_configuration_name" {
+  type        = string
+  description = "value"
+  default     = "SQL_Default"
+
+}
+variable "max_size_gb" {
+  type        = number
+  description = "value"
+
+}
+variable "sku_name" {
+  type        = string
+  description = "value"
+
+}
+variable "storage_account_type" {
+  type        = string
+  description = "value"
+  default     = "Geo"
+
+}
+variable "name" {
+    type = string
+    description = "value"
+
+}
+# variable "" {
+#     type = string
+#     description = "value"
+
+# }
