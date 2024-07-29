328787e710985ef73fca12f6290803520d7f91cf
lalita2407 <lalitalallu02@gmail.com>
2024-07-29 13:37:54 +0530

    updated module added private endpoint resource

--- main.tf
+++ main.tf
@@ -68,3 +68,31 @@ resource "azurerm_key_vault_secret" "mssql_password" {
 
   depends_on = [azurerm_mssql_server.example]
 }  
+
+# Creates  a private endpoint with private dns
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = azurerm_mssql_server.example.id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  depends_on = [azurerm_mssql_server.example]
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -84,3 +84,37 @@ variable "keyvault_name" {
  type        = string
   
 }
+# PE
+# private endpoint
+variable "pename" {
+  type        = string
+  description = "Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created."
+
+}
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+  default = [ "sqlServer" ]
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
+}
