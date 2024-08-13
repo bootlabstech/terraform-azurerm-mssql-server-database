6542620685219f10bff7446ce904ffa3938a43e0
lalita2407 <lalitalallu02@gmail.com>
2024-08-13 14:43:47 +0530

    latest

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 # Creates a  MsSQL Server
+# Creates a  MsSQL Server
 resource "azurerm_mssql_server" "example" {
-  name                         = var.server_name
+  name                         = var.name
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = var.server_version
   administrator_login          = var.administrator_login
-  administrator_login_password = var.administrator_login_password
+  administrator_login_password = random_password.password.result
   connection_policy            = var.connection_policy
   public_network_access_enabled = var.public_network_access_enabled
   lifecycle {
@@ -14,9 +15,66 @@ resource "azurerm_mssql_server" "example" {
     ]
   }
 }
+
+
+# get key vault details to store DB password as secret
+data "azurerm_key_vault" "key_vault" {
+  name                = var.keyvault_name
+  resource_group_name = var.resource_group_name
+}
+
+# Creates random password
+resource "random_password" "password" {
+  length      = 12
+  lower       = true
+  min_lower   = 6
+  min_numeric = 2
+  min_special = 2
+  min_upper   = 2
+  numeric     = true
+  special     = true
+  upper       = true
+
+}
+
+# Stores DB login password as keyvault secret
+resource "azurerm_key_vault_secret" "mssql_password" {
+  name         = "${var.name}-pwd"
+  value        = random_password.password.result
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+
+  depends_on = [azurerm_mssql_server.example]
+}  
+# Creates  a private endpoint with private dns
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = azurerm_mssql_server.example.id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [ azurerm_mssql_server.example ]
+
+}
 # Creates a mssql database
 resource "azurerm_mssql_database" "test" {
-  name                           = var.name
+  name                           = var.dbname
   server_id                      = azurerm_mssql_server.example.id
   create_mode                    = var.create_mode
   geo_backup_enabled             = var.geo_backup_enabled
@@ -44,12 +102,5 @@ resource "azurerm_mssql_database" "test" {
   #   license_type   = "LicenseIncluded"
   #   read_scale     = true
   #   zone_redundant = true
+  depends_on = [ azurerm_mssql_server.example ]
 }
-
-# # Network setting allows All Azure Services
-# resource "azurerm_mssql_firewall_rule" "example" {
-#   name             = "FirewallRule1"
-#   server_id        = azurerm_mssql_server.example.id
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "0.0.0.0"
-# }
--- variables.tf
+++ variables.tf
@@ -1,41 +1,77 @@
 # server
-variable "server_name" {
+variable "name" {
   type        = string
-  description = "value"
+  description = "The name of the Microsoft SQL Server. This needs to be globally unique within Azure. "
 
 }
 variable "resource_group_name" {
   type        = string
-  description = "value"
+  description = "The name of the resource group in which to create the Microsoft SQL Server."
 
 }
 variable "location" {
   type        = string
-  description = "value"
+  description = "Specifies the supported Azure location where the resource exists.Central India or South India"
 
 }
 variable "server_version" {
   type        = string
-  description = "value"
+  description = "The version for the new server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
   default     = "12.0"
 
 }
 variable "administrator_login" {
   type        = string
-  description = "value"
-
-}
-variable "administrator_login_password" {
-  type        = string
-  description = "value"
-
+  description = "The administrator login name for the new server."
 
 }
 variable "connection_policy" {
   type        = string
-  description = "value"
+  description = "The connection policy the server will use. Possible values are Default, Proxy, and Redirect. Defaults to Default."
   default     = "Default"
 
+}
+variable "public_network_access_enabled" {
+    type = bool
+    description = "Whether public network access is allowed for this server. Defaults to true."
+    default = false
+
+}
+
+variable "keyvault_name" {
+  type = string
+  
+}
+
+# PE
+# private endpoint
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+  default = [ "sqlServer" ]
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
 }
 variable "create_mode" {
   type        = string
@@ -73,7 +109,7 @@ variable "storage_account_type" {
 
 }
 # database
-variable "name" {
+variable "dbname" {
     type = string
     description = "value"
 
