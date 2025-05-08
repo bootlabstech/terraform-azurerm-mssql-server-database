7c3ce1f53b65c2a9277e2b8ea626a2777215daad
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-08 14:15:49 +0530

    updated the mssql config and variables

--- main.tf
+++ main.tf
@@ -5,44 +5,17 @@ resource "azurerm_mssql_server" "example" {
   location                     = var.location
   version                      = var.server_version
   administrator_login          = var.administrator_login
-  administrator_login_password = random_password.password.result
+  administrator_login_password = var.administrator_login_password
   connection_policy            = var.connection_policy
   public_network_access_enabled = var.public_network_access_enabled
+
   lifecycle {
-    ignore_changes = [
+    ignore_changes = [ 
       tags,
-    ]
+     ]
   }
 }
 
-# get key vault details to store DB password as secret
-data "azurerm_key_vault" "key_vault" {
-  name                = var.keyvault_name
-  resource_group_name = var.resource_group_name
-}
-
-# Creates random password
-resource "random_password" "password" {
-  length      = 12
-  lower       = true
-  min_lower   = 6
-  min_numeric = 2
-  min_special = 2
-  min_upper   = 2
-  numeric     = true
-  special     = true
-  upper       = true
-
-}
-
-# Stores DB login password as keyvault secret
-resource "azurerm_key_vault_secret" "mssql_password" {
-  name         = "${var.name}-pwd"
-  value        = random_password.password.result
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-
-  depends_on = [azurerm_mssql_server.example]
-}  
 # Creates a mssql database
 resource "azurerm_mssql_database" "test" {
   name                           = var.name
@@ -53,10 +26,33 @@ resource "azurerm_mssql_database" "test" {
   max_size_gb                    = var.max_size_gb
   sku_name                       = var.sku_name
   storage_account_type           = var.storage_account_type
+
   lifecycle {
-    ignore_changes = [
+    ignore_changes = [ 
       tags,
-    ]
+     ]
   }
-  depends_on = [ azurerm_mssql_server.example ]
+
+  #   threat_detection_policy {
+  #     disabled_alerts = [ "value" ]
+  #     email_account_admins = 
+  #     email_addresses = 
+  #     retention_days = 
+  #     state = 
+  #     storage_account_access_key = 
+  #     storage_endpoint = 
+
+  #   }
+  #   collation      = "SQL_Latin1_General_CP1_CI_AS"
+  #   license_type   = "LicenseIncluded"
+  #   read_scale     = true
+  #   zone_redundant = true
+}
+
+# Network setting allows All Azure Services
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
--- variables.tf
+++ variables.tf
@@ -1,74 +1,41 @@
 # server
-variable "name" {
-  type        = string
-  description = "The name of the Microsoft SQL Server. This needs to be globally unique within Azure. "
-
-}
 variable "server_name" {
   type        = string
   description = "value"
+
 }
 variable "resource_group_name" {
   type        = string
-  description = "The name of the resource group in which to create the Microsoft SQL Server."
+  description = "value"
 
 }
 variable "location" {
   type        = string
-  description = "Specifies the supported Azure location where the resource exists.Central India or South India"
+  description = "value"
 
 }
 variable "server_version" {
   type        = string
-  description = "The version for the new server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
+  description = "value"
   default     = "12.0"
 
 }
 variable "administrator_login" {
   type        = string
-  description = "The administrator login name for the new server."
+  description = "value"
+
+}
+variable "administrator_login_password" {
+  type        = string
+  description = "value"
+
 
 }
 variable "connection_policy" {
   type        = string
-  description = "The connection policy the server will use. Possible values are Default, Proxy, and Redirect. Defaults to Default."
+  description = "value"
   default     = "Default"
 
-}
-
-variable "keyvault_name" {
-  type = string
-  
-}
-
-# private endpoint
-
-variable "private_endpoint_subnet_id" {
-  type        = string
-  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
-
-}
-
-variable "is_manual_connection" {
-  type        = bool
-  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
-  default     = false
-
-}
-variable "subresource_names" {
-  type        = list(string)
-  description = " A list of subresource names which the Private Endpoint is able to connect to."
-  default = [ "sqlServer" ]
-}
-variable "private_dns_zone_ids" {
-  type        = list(string)
-  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
-}
-
-variable "ip_configuration" {
-  type        = bool
-  default     = false
-  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
 }
 variable "create_mode" {
   type        = string
@@ -106,7 +73,7 @@ variable "storage_account_type" {
 
 }
 # database
-variable "dbname" {
+variable "name" {
     type = string
     description = "value"
 
@@ -114,6 +81,6 @@ variable "dbname" {
 variable "public_network_access_enabled" {
     type = bool
     description = "value"
-    default = false
+    default = true
 
 }
